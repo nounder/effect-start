2960c4a749954a111f4c6d02b8782044bd033c75
Rafael Gutkowski <nounderline@libred.org>
2024-12-08 05:05:13 +0700

    workaround vite dev server being closed on every request
    will have to create a runtime on my own
    there is none for deno, only node and bun.
    i'd have to reimplement proxy code for IncomingMessage
    and ServerResponse. after spending few hours on supporting
    Request and Response in deno, i don't want to deal with it now.

--- src/vite/effect.ts
+++ src/vite/effect.ts
@@ -6,71 +6,66 @@ import {
   HttpServerRequest,
   HttpServerResponse,
 } from "@effect/platform"
+import { IncomingMessage, ServerResponse } from "node:http"
+import { Socket } from "node:net"
 
 export class Vite extends Context.Tag("Vite")<Vite, {
   server: ViteDevServer
   handler: (req: Request) => Promise<Response> | Response
 }>() {}
 
-export const ViteDev = Layer.effect(
+export const ViteDev = Layer.scoped(
   Vite,
-  Effect.tryPromise(async () => {
-    if (globalThis.cachedViteDevServer) {
-      const server = globalThis.cachedViteDevServer
+  Effect.acquireRelease(
+    Effect.tryPromise(async () => {
+      if (globalThis.cachedViteDevServer) {
+        return globalThis.cachedViteDevServer
+      }
+
+      const config = await createViteConfig()
+      const server = await createServer(config)
       const handler = await createViteDevHandler(server)
 
-      return {
+      return globalThis.cachedViteDevServer = {
         server,
         handler,
       }
-    }
+    }),
+    (vite) =>
+      Effect.gen(function* () {
+        if (globalThis.cachedViteDevServer) {
+          return
+        }
 
-    const config = await createViteConfig()
-    const server = await createServer(config)
-    const handler = await createViteDevHandler(server)
-
-    globalThis.cachedViteDevServer = server
-
-    return {
-      server,
-      handler,
-    }
-  }).pipe(
-    // Make sure server is closed
-    Effect.tap((vite) =>
-      Effect.addFinalizer(() =>
-        Effect.promise(async () => {
+        yield* Effect.log("Closing Vite Dev Server...")
+        yield* Effect.promise(async () => {
           await vite.server.waitForRequestsIdle()
           await vite.server.close()
         })
-      )
-    ),
+      }),
   ),
 )
 
 export const ViteDevHttpRouteHandler = Effect.gen(function* () {
   const vite = yield* Vite
   const req = yield* HttpServerRequest.HttpServerRequest
-  const fetchHandler = yield* Effect.promise(() =>
-    createViteDevHandler(vite.server)
-  )
-  const fetchReq = req.source
+  const sourceReq = req.source
 
-  if (!(fetchReq instanceof Request)) {
-    throw new Error("request must be standard web Request")
-  }
-
-  const res = yield* Effect.tryPromise(() =>
-    Promise.resolve(
-      fetchHandler(fetchReq)
+  if (sourceReq instanceof Request) {
+    const res = yield* Effect.tryPromise(() =>
+      Promise.resolve(vite.handler(sourceReq.clone()))
         // Effect seems to drain response stream twice
         // causing vite to return gateway error for all
         // subsequent requests
-        .then((v) => v.clone()),
+        .then((res) => res.clone())
     )
-  )
 
-  return HttpServerResponse.raw(res.body, {
-    headers: Headers.fromInput(res.headers as any),
-  })
+    // todo: cookies are not passed?
+    return HttpServerResponse.raw(res.body, {
+      status: res.status,
+      headers: Headers.fromInput(res.headers as any),
+    })
+  }
+
+  throw new Error("Invalid request")
 })
