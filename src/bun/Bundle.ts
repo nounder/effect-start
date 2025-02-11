f7bd61bc4bcdf8498a03f1fff2e2d68aa1fa73cd
Rafael Gutkowski <nounderline@libred.org>
2025-02-11 01:42:59 -0800

    Fix SSR build
    We cannot externalie solid because it would use
    export with incorrect condition, ie. one used in root bun program.

--- src/bun/Bundle.ts
+++ src/bun/Bundle.ts
@@ -1,29 +1,31 @@
-import { FileSystem, HttpApp } from "@effect/platform"
-import {
-  Console,
-  Effect,
-  pipe,
-  Ref,
-  Stream,
-  SubscriptionRef,
-  SynchronizedRef,
-} from "effect"
+import { HttpApp } from "@effect/platform"
+import { Console, Effect, pipe, Ref, Stream, SubscriptionRef } from "effect"
 import * as NodeFS from "node:fs/promises"
 import * as NodePath from "node:path"
 import * as NodeUrl from "node:url"
-import { watchNodeWithOptions } from "../effect/node.ts"
 
 const SOURCE_FILENAME = /.*\.(tsx?|jsx?)$/
 
 async function bundleHttpApp<M extends { default: any }>(
   module: string,
 ): Promise<HttpApp.Default> {
+  const packageJson = await import("../../package.json", {
+    with: { type: "json" },
+  })
   const output = await Bun.build({
     entrypoints: [module],
     target: "bun",
     conditions: ["solid"],
     sourcemap: "inline",
-    packages: "external",
+    packages: "bundle",
+    external: [
+      ...Object.keys(packageJson.dependencies)
+        .filter(v => v !== "solid-js" && v !== "@solidjs/router")
+        .flatMap(v => [
+          v,
+          v + "/*",
+        ]),
+    ],
     plugins: [
       await import("bun-plugin-solid").then((v) =>
         v.SolidPlugin({
@@ -41,11 +43,11 @@ async function bundleHttpApp<M extends { default: any }>(
   const file = Bun.file(path)
   await file.write(contents)
 
-  const bundleModule = await import(path)
-
   console.log(path)
 
-  // await file.delete()
+  const bundleModule = await import(path)
+
+  await file.delete()
 
   return bundleModule.default
 }
@@ -54,10 +56,16 @@ export const build = <M extends { default: any }>(module: string) =>
   Effect.gen(function*() {
     const modulePath = NodeUrl.fileURLToPath(module)
     const baseDir = NodePath.dirname(modulePath)
-    const bundle = Effect.tryPromise(() => bundleHttpApp(modulePath))
+    const bundleEffect = Effect.tryPromise({
+      try: () => bundleHttpApp(modulePath),
+      catch: (err) => {
+        console.trace()
+        throw err
+      },
+    })
 
     const ref = yield* SubscriptionRef.make<HttpApp.Default>(
-      yield* bundle,
+      yield* bundleEffect,
     )
 
     const fileChanges = pipe(
@@ -78,7 +86,7 @@ export const build = <M extends { default: any }>(module: string) =>
         strategy: "enforce",
       }),
       Stream.runForEach(() =>
-        bundle.pipe(Effect.flatMap(app => Ref.update(ref, () => app)))
+        bundleEffect.pipe(Effect.flatMap(app => Ref.update(ref, () => app)))
       ),
     )
 
