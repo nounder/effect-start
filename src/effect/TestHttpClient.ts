798433bca209e8fd964a979913829319bd6111d6
Rafael Gutkowski <nounderline@libred.org>
2025-03-16 18:43:43 -0400

    Automatically set base URL

--- src/effect/TestHttpClient.ts
+++ src/effect/TestHttpClient.ts
@@ -2,11 +2,21 @@ import {
   HttpApp,
   HttpClient,
   HttpClientError,
+  HttpClientRequest,
   HttpClientResponse,
   HttpServerRequest,
   HttpServerResponse,
 } from "@effect/platform"
-import { Context, Effect, FiberRef, Layer, pipe, Scope, Stream } from "effect"
+import {
+  Context,
+  Effect,
+  FiberRef,
+  identity,
+  Layer,
+  pipe,
+  Scope,
+  Stream,
+} from "effect"
 
 const WebHeaders = globalThis.Headers
 
@@ -19,52 +29,62 @@ class TestHttpApp
 
 export const make = <E = never>(
   httpApp?: HttpApp.Default<E>,
+  opts?: {
+    baseUrl?: string | null
+  },
 ): HttpClient.HttpClient.With<
   HttpClientError.HttpClientError | E,
   Scope.Scope
 > =>
-  HttpClient.make(
-    (request, url, signal, fiber) => {
-      const context = fiber.getFiberRef(FiberRef.currentContext)
-      const app: HttpApp.Default<any> = httpApp
-        ?? context.unsafeMap.get(TestHttpApp.key)
-      const send = (body: BodyInit | undefined) => {
-        const serverRequest = HttpServerRequest.fromWeb(
-          new Request(url.toString(), {
-            method: request.method,
-            headers: new WebHeaders(request.headers),
-            body,
-            duplex: request.body._tag === "Stream" ? "half" : undefined,
-            signal,
-          } as any),
-        )
-
-        return pipe(
-          app,
-          Effect.provideService(
-            HttpServerRequest.HttpServerRequest,
-            serverRequest,
-          ),
-          Effect.andThen(HttpServerResponse.toWeb),
-          Effect.andThen(res => HttpClientResponse.fromWeb(request, res)),
-        )
-      }
-
-      switch (request.body._tag) {
-        case "Raw":
-        case "Uint8Array":
-          return send(request.body.body as any)
-        case "FormData":
-          return send(request.body.formData)
-        case "Stream":
-          return Effect.flatMap(
-            Stream.toReadableStreamEffect(request.body.stream),
-            send,
+  pipe(
+    HttpClient.make(
+      (request, url, signal, fiber) => {
+        const context = fiber.getFiberRef(FiberRef.currentContext)
+        const app: HttpApp.Default<any> = httpApp
+          ?? context.unsafeMap.get(TestHttpApp.key)
+        const send = (body: BodyInit | undefined) => {
+          const serverRequest = HttpServerRequest.fromWeb(
+            new Request(url.toString(), {
+              method: request.method,
+              headers: new WebHeaders(request.headers),
+              body,
+              duplex: request.body._tag === "Stream" ? "half" : undefined,
+              signal,
+            } as any),
           )
-      }
 
-      return send(undefined)
-    },
+          return pipe(
+            app,
+            Effect.provideService(
+              HttpServerRequest.HttpServerRequest,
+              serverRequest,
+            ),
+            Effect.andThen(HttpServerResponse.toWeb),
+            Effect.andThen(res => HttpClientResponse.fromWeb(request, res)),
+          )
+        }
+
+        switch (request.body._tag) {
+          case "Raw":
+          case "Uint8Array":
+            return send(request.body.body as any)
+          case "FormData":
+            return send(request.body.formData)
+          case "Stream":
+            return Effect.flatMap(
+              Stream.toReadableStreamEffect(request.body.stream),
+              send,
+            )
+        }
+
+        return send(undefined)
+      },
+    ),
+    opts?.baseUrl === null
+      ? identity
+      : HttpClient.mapRequest(
+        HttpClientRequest.prependUrl(opts?.baseUrl ?? "http://localhost"),
+      ),
   )
 
 const layerClient = make()
