7173581955f6f658e76fbb05596a6824ae7414f5
Rafael Gutkowski <nounderline@libred.org>
2025-04-22 15:40:28 +0200

    Remove Vite code
    Hopefully, it will be revived somewhen!

--- src/vite/Vite.ts
+++
@@ -1,7 +0,0 @@
-import { Context } from "effect"
-
-export class Vite extends Context.Tag("Vite")<Vite, {
-  fetch: (req: Request) => Promise<Response> | Response
-  // this may not be necssary?
-  manifest?: any
-}>() {}
