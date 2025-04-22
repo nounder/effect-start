910d219fe593550e86548c41629e1c066282e8c8
Rafael Gutkowski <nounderline@libred.org>
2025-04-22 15:31:18 +0200

    Clean up repo structure

--- pad/250125-bun-build.ts
+++
@@ -1,9 +0,0 @@
-const out = await Bun.build({
-  outdir: "dst",
-  entrypoints: [
-    Bun.fileURLToPath(import.meta.resolve("../src/entry-client.tsx")),
-  ],
-  naming: "[name]:[hash].[ext]",
-})
-
-console.log(out)
