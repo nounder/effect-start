4942e72d952789baf234f199cc9d3d23bc312bf9
Rafael Gutkowski <nounderline@libred.org>
2025-05-27 14:35:30 -0400

    Add Tanstack router typings

--- examples/bun-react-tanstack-router/src/client.tsx
+++ examples/bun-react-tanstack-router/src/client.tsx
@@ -17,6 +17,12 @@ const router = createRouter({
   history: createHashHistory(),
 })
 
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 export function App() {
   return <RouterProvider router={router} />
 }
