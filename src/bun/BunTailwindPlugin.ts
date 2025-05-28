9505a2acedbd8da2a1380d957f4e550df554f238
Rafael Gutkowski <nounderline@libred.org>
2025-05-27 23:42:25 -0400

    Scoped Tailwind compilation
    CSS files that import Tailwind now only include classes
    that are used by files that import that CSS files.

--- src/bun/BunTailwindPlugin.ts
+++ src/bun/BunTailwindPlugin.ts
@@ -1,17 +1,29 @@
 import * as Tailwind from "@tailwindcss/node"
-import type {
-  BunPlugin,
-} from "bun"
-import {
-  Array,
-} from "effect"
+import type { BunPlugin } from "bun"
+import { Iterable } from "effect"
+
+type Compiler = Awaited<ReturnType<typeof Tailwind.compile>>
 
 export const make = (opts: {
+  /**
+   * Custom importer function to load Tailwind.
+   * By default, it imports from '@tailwindcss/node'.
+   * If you want to use a different version or a custom implementation,
+   * provide your own importer.
+   */
   importer?: () => Promise<typeof Tailwind>
+  /**
+   * Patterm to match component and HTML files for class name extraction.
+   */
   filesPattern?: RegExp
+  /**
+   * Pattern to match CSS files that import Tailwind.
+   */
+  cssPattern?: RegExp
 } = {}): BunPlugin => {
   const {
     filesPattern = /\.(tsx|jsx|html|svelte|vue|astro)$/,
+    cssPattern = /\.css$/,
     importer = () =>
       import("@tailwindcss/node").catch(err => {
         throw new Error(
@@ -24,21 +36,56 @@ export const make = (opts: {
     name: "Bun Tailwind.css plugin",
     async setup(builder) {
       const Tailwind = await importer()
-      const defaultInputCss = `@import "tailwindcss";`
-      let twCompiler: Awaited<ReturnType<typeof Tailwind.compile>>
-      let build: string | null = null
 
-      const collectedClassNames = new Set<string>()
+      // (file) -> (class names)
+      const classNameCandidates = new Map<string, Set<string>>()
+      // (importer path) -> (imported paths)
+      const importAncestors = new Map<string, Set<string>>()
+      // (imported path) -> (importer paths)
+      const importDescendants = new Map<string, Set<string>>()
 
-      builder.onStart(async () => {
-        twCompiler = await Tailwind.compile(defaultInputCss, {
-          base: process.cwd(),
-          onDependency: (path) => {},
-        })
+      /**
+       * Track import relationships.
+       * We do this to scope all class name candidates to tailwind entrypoints
+       */
+      builder.onResolve({
+        filter: /.*/,
+      }, (args) => {
+        const fullPath = Bun.resolveSync(args.path, args.resolveDir)
+
+        if (fullPath.includes("/node_modules/")) {
+          return undefined
+        }
+
+        /**
+         * Register every visited module.
+         */
+        {
+          if (!importAncestors.has(fullPath)) {
+            importAncestors.set(fullPath, new Set())
+          }
+
+          if (!importDescendants.has(fullPath)) {
+            importDescendants.set(fullPath, new Set())
+          }
+
+          if (!importAncestors.has(args.importer)) {
+            importAncestors.set(args.importer, new Set())
+          }
+
+          if (!importDescendants.has(args.importer)) {
+            importDescendants.set(args.importer, new Set())
+          }
+        }
+
+        importAncestors.get(fullPath)!.add(args.importer)
+        importDescendants.get(args.importer)!.add(fullPath)
+
+        return undefined
       })
 
       /**
-       * Extract class names from the source code.
+       * Scan for class name candidates in component files.
        */
       builder.onLoad({
         filter: filesPattern,
@@ -46,30 +93,67 @@ export const make = (opts: {
         const contents = await Bun.file(args.path).text()
         const classNames = extractClassNames(contents)
 
-        classNames.forEach((className) => collectedClassNames.add(className))
+        if (classNames.size > 0) {
+          classNameCandidates.set(args.path, classNames)
+        }
 
         return undefined
       })
 
-      builder.onResolve({
-        filter: /^tailwindcss$/,
-      }, (args) => {
-        return {
-          path: `${args.resolveDir}/${args.path}`,
-        }
-      })
-
+      /**
+       * Compile tailwind entrypoints.
+       */
       builder.onLoad({
-        filter: /^tailwindcss$/,
+        filter: cssPattern,
       }, async (args) => {
+        const source = await Bun.file(args.path).text()
+
+        // Bypass CSS files that do not import 'tailwindcss'
+        if (!hasTailwindImport(source)) {
+          return undefined
+        }
+
+        const compiler = await Tailwind.compile(source, {
+          base: process.cwd(),
+          onDependency: (path) => {},
+        })
+
+        // wait for other files to be loaded so we can collect class name candidates
+        // NOTE: at currently processed css won't be in import graph because
+        // we haven't returned its contents yet.
         await args.defer()
 
-        build = twCompiler.build([
-          ...collectedClassNames,
-        ])
+        const candidates = new Set<string>()
+        {
+          const pendingModules = [
+            // get class name candidates from all modules that import this one
+            ...(importAncestors.get(args.path) ?? []),
+          ]
+          const visitedModules = new Set<string>()
+
+          while (pendingModules.length > 0) {
+            const currentPath = pendingModules.shift()!
+
+            if (visitedModules.has(currentPath)) {
+              continue
+            }
+
+            const moduleImports = importDescendants.get(currentPath)
+
+            moduleImports?.forEach(moduleImport => {
+              const moduleCandidates = classNameCandidates.get(moduleImport)
+
+              moduleCandidates?.forEach(candidate => candidates.add(candidate))
+
+              pendingModules.push(moduleImport)
+            })
+          }
+        }
+
+        const contents = compiler.build([...candidates])
 
         return {
-          contents: build!,
+          contents,
           loader: "css",
         }
       })
@@ -77,7 +161,10 @@ export const make = (opts: {
   }
 }
 
-function extractClassNames(source: string): string[] {
+function hasTailwindImport(css: string): boolean {
+  return /@import\s+(url\()?["']?[^"')]+["']?\)?\s*[^;]*;/.test(css)
+}
+
   const classNames = new Set<string>()
 
   // Match class attributes
