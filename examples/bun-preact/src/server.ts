6d6aa6d62a74ceabfd5916547d3ea35b11093dd6
Rafael Gutkowski <nounderline@libred.org>
2025-07-08 12:30:53 -0400

    Use Start API in bun-preact example

--- examples/bun-preact/src/server.ts
+++ examples/bun-preact/src/server.ts
@@ -1,85 +1,11 @@
-import {
-  FetchHttpClient,
-  HttpRouter,
-  HttpServer,
-  HttpServerResponse,
-} from "@effect/platform"
-import {
-  BunContext,
-  BunHttpServer,
-  BunRuntime,
-} from "@effect/platform-bun"
-import {
-  Effect,
-  Layer,
-  pipe,
-} from "effect"
-import {
-  BundleHttp,
-  FileRouter,
-  HttpAppExtra,
-} from "effect-bundler"
-import {
-  BunBundle,
-  BunTailwindPlugin,
-} from "effect-bundler/bun"
-import IndexHtml from "./index.html" with { type: "file" }
+import { Layer } from "effect"
+import { Start } from "effect-bundler"
 
-const BundlePath = "/_bundle"
-
-export const App = HttpRouter.empty.pipe(
-  HttpRouter.get(
-    "/",
-    BundleHttp.entrypoint(),
-  ),
-  HttpRouter.mountApp(
-    BundlePath,
-    BundleHttp.httpApp(),
-  ),
-  HttpRouter.get(
-    "/hello",
-    HttpServerResponse.text("Hello World!"),
-  ),
+export default Layer.mergeAll(
+  // Start.router(() => import("./routes/_manifest")),
+  Start.bundleClient("src/index.html"),
 )
 
-const ClientBundle = BunBundle
-  .bundleClient({
-    entrypoints: [
-      IndexHtml,
-    ],
-    conditions: ["development"],
-    define: {
-      NODE_ENV: "development",
-    },
-    publicPath: `${BundlePath}/`,
-    plugins: [
-      BunTailwindPlugin.make(),
-    ],
-  })
-
-export const layerServer = () =>
-  pipe(
-    HttpServer.serve(App.pipe(
-      Effect.catchAll(HttpAppExtra.renderError),
-    )),
-    HttpServer.withLogAddress,
-    Layer.provide([
-      FetchHttpClient.layer,
-      BunHttpServer.layer({
-        port: 3000,
-      }),
-      FileRouter.layer(import.meta.resolve("./routes")),
-    ]),
-  )
-
 if (import.meta.main) {
-  pipe(
-    layerServer(),
-    Layer.provide([
-      ClientBundle.devLayer,
-      BunContext.layer,
-    ]),
-    Layer.launch,
-    BunRuntime.runMain,
-  )
+  Start.serve(() => import("./server"))
 }
