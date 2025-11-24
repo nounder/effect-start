7c3f3194c2522f4764b850f7cbc0073061e25fcc
Rafael Gutkowski <nounderline@libred.org>
2025-11-24 20:08:59 +0100

    remove examples: bun-preact bun-react-spa bun-react-tanstack-router

--- examples/bun-react-tanstack-router/src/routes/api/$/_server.ts
+++
@@ -1,6 +0,0 @@
-import { HttpServerResponse } from "@effect/platform"
-import { Effect } from "effect"
-
-export default Effect.gen(function*() {
-  return HttpServerResponse.text("I'm API.")
-})
--- examples/bun-react-tanstack-router/src/routes/users/_page.tsx
+++
@@ -1,9 +0,0 @@
-export default function() {
-  return (
-    <div>
-      <h1>
-        Users Section
-      </h1>
-    </div>
-  )
-}
