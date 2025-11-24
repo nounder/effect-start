5514013cbd551d142ef1ad5c937a779f3b270eca
Rafael Gutkowski <nounderline@libred.org>
2025-11-24 21:25:19 +0100

    test bun fullstack server

--- examples/bun-fullstack/src/server.ts
+++ examples/bun-fullstack/src/server.ts
@@ -1,9 +1,18 @@
 import { Start } from "effect-start"
+import { BunHttpServer } from "effect-start/bun"
 
 export default Start.layer(
   Start.router({
     load: () => import("./routes"),
     path: import.meta.resolve("./routes"),
+  }),
+  BunHttpServer.layer({
+    port: 3010,
+    routes: {
+      "/data": Response.json({
+        message: "hello /data",
+      }),
+    },
   }),
 )
 
