e33dd169cff402a1a7daa92b12c44c153597cc79
Rafael Gutkowski <nounderline@libred.org>
2025-11-28 17:24:54 +0100

    remove movies example

--- examples/movies/src/client.ts
+++
@@ -1,1 +0,0 @@
-// initialize posthog or whatever
--- examples/movies/src/routes/movies/[id]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { Route } from "effect-start"
-
-export default Route.html(function*(ctx) {
-  return (
-    <div>
-      Movie ID from URL: {ctx.url.pathname}
-    </div>
-  )
-})
