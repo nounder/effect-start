c46cf0b626f2ad644f9baf92e1218a67df4aa521
Rafael Gutkowski <nounderline@libred.org>
2025-12-18 14:00:10 +0100

    intro Action

---
+++ src/router/Action.test.ts
@@ -0,0 +1,147 @@
+import * as test from "bun:test"
+import {
+  Effect,
+  Schema,
+} from "effect"
+import * as Action from "./Action.ts"
+
+test.it("infers schema", async () => {
+  test.expect.assertions(2)
+
+  const schemaHeaders = Schema.Struct({
+    "x-hello": Schema.String,
+  })
+  const headers = {
+    "x-hello": "test-value",
+  }
+  type ExpectedBindings = {
+    headers: typeof headers
+  }
+
+  const action = Action.empty.pipe(
+    Action.schemaHeaders(schemaHeaders),
+    Action.text((action) =>
+      Effect.gen(function*() {
+        test
+          .expectTypeOf(action)
+          .toExtend<ExpectedBindings>()
+
+        test
+          .expect(action)
+          .toMatchObject({
+            headers,
+          })
+
+        return "Hello, World!"
+      })
+    ),
+  )
+
+  test
+    .expectTypeOf(action)
+    .toExtend<
+      Action.ActionSet.ActionSet<[
+        Action.Action.Action<
+          Action.ActionHandler<void, never, never>,
+          ExpectedBindings
+        >,
+        Action.Action.Action<
+          Action.ActionHandler<string, never, never>,
+          ExpectedBindings
+        >,
+      ]>
+    >()
+  test
+    .expectTypeOf<Action.Action.Bindings<typeof action>>()
+    .toExtend<ExpectedBindings>()
+
+  const result = await runAction(action, {
+    headers,
+  })
+
+  test
+    .expect(result)
+    .toBe("Hello, World!")
+})
+
+test.it("uses GET method", async () => {
+  test.expect.assertions(2)
+
+  type ExpectedBindings = {
+    method: "GET"
+  }
+
+  const action = Action.get(
+    Action.text((action) =>
+      Effect.gen(function*() {
+        test
+          .expectTypeOf(action)
+          .toExtend<ExpectedBindings>()
+        test
+          .expect(action)
+          .toMatchObject({
+            method: "GET",
+          })
+
+        return "Hello, World!"
+      })
+    ),
+  )
+
+  test
+    .expectTypeOf(action)
+    .toExtend<
+      Action.ActionSet.ActionSet<[
+        Action.Action.Action<
+          Action.ActionHandler<void, never, never>,
+          ExpectedBindings
+        >,
+        Action.Action.Action<
+          Action.ActionHandler<string, never, never>,
+          ExpectedBindings
+        >,
+      ]>
+    >()
+  test
+    .expectTypeOf<Action.Action.Bindings<typeof action>>()
+    .toExtend<ExpectedBindings>()
+
+  const result = await runAction(action, {
+    method: "GET",
+  })
+
+  test
+    .expect(result)
+    .toBe("Hello, World!")
+})
+
+test.it("uses GET & POST method", async () => {
+  test.expect.assertions(1)
+
+  type ExpectedBindings =
+    | { method: "GET" }
+    | { method: "POST" }
+
+  const action = Action.merge(
+    Action.get(Action.text(() => Effect.succeed("get"))),
+    Action.post(Action.text(() => Effect.succeed("post"))),
+  )
+
+  test
+    .expectTypeOf<Action.Action.Bindings<typeof action>>()
+    .toExtend<ExpectedBindings>()
+
+  const allActions = [...action]
+  test.expect(allActions.length).toBe(4)
+})
+
+function runAction(
+  actionSet: Action.ActionSet.Any,
+  bindings: unknown,
+) {
+  const actions = [...actionSet]
+  const action = actions[actions.length - 1]
+  return Effect.runPromise(
+    action.handler(bindings) as Effect.Effect<unknown>,
+  )
+}
---
+++ src/router/Action.ts
@@ -0,0 +1,326 @@
+import * as Effect from "effect/Effect"
+import { dual } from "effect/Function"
+import * as Pipeable from "effect/Pipeable"
+import * as Predicate from "effect/Predicate"
+import * as Schema from "effect/Schema"
+
+type ActionPattern = {
+  protocol:
+    | "http"
+    | "*"
+  method:
+    | "GET"
+    | "POST"
+    | "PUT"
+  path: `/${string}`
+}
+
+const ActionSetItems: unique symbol = Symbol()
+
+export const TypeId: unique symbol = Symbol.for("effect-start/ActionSet")
+
+export type Item = Action.Default | ActionSet.Any
+export type ItemArray = ReadonlyArray<Item>
+
+export namespace ActionSet {
+  export type ActionSet<M extends ItemArray = []> =
+    & Data<M>
+    & {
+      [TypeId]: typeof TypeId
+    }
+    & Pipeable.Pipeable
+    & Iterable<Action.Default>
+
+  export type Data<M extends ItemArray = []> = {
+    [ActionSetItems]: M
+  }
+
+  export type Default = ActionSet<readonly [Item, ...Item[]]>
+
+  export type Any = ActionSet<ItemArray>
+
+  export type Items<T extends Data<any>> = T extends Data<infer M> ? M
+    : never
+
+  export type Bindings<T extends Data<any>> = T extends Data<infer M>
+    ? _ExtractBindings<M>
+    : never
+
+  type _ExtractBindings<M extends ItemArray> = M extends readonly [
+    infer Head,
+    ...infer Tail extends ItemArray,
+  ] ? (
+      Head extends Action.Action<any, infer B> ? B & _ExtractBindings<Tail>
+        : Head extends ActionSet<infer Nested>
+          ? _ExtractBindings<Nested> & _ExtractBindings<Tail>
+        : _ExtractBindings<Tail>
+    )
+    : {}
+}
+
+const Proto = {
+  [TypeId]: TypeId,
+  pipe() {
+    return Pipeable.pipeArguments(this, arguments)
+  },
+  *[Symbol.iterator](this: ActionSet.Any) {
+    for (const item of this[ActionSetItems]) {
+      yield* item
+    }
+  },
+}
+
+export function isActionSet(
+  input: unknown,
+): input is ActionSet.Any {
+  return Predicate.hasProperty(input, TypeId)
+}
+
+export function make<M extends ItemArray = []>(
+  items: M = [] as unknown as M,
+): ActionSet.ActionSet<M> {
+  return Object.assign(
+    Object.create(Proto),
+    {
+      [ActionSetItems]: items,
+    },
+  ) as ActionSet.ActionSet<M>
+}
+
+export const empty: ActionSet.ActionSet<[]> = make()
+
+export function items<T extends ActionSet.Data<any>>(
+  self: T,
+): ActionSet.Items<T> {
+  return self[ActionSetItems]
+}
+
+function makeAction<H extends ActionHandler, B extends Record<string, any>>(
+  handler: H,
+): Action.Action<H, B> {
+  const action: Action.Action<H, B> = Object.assign(
+    Object.create(Proto),
+    {
+      [ActionSetItems]: [],
+      handler,
+      *[Symbol.iterator]() {
+        yield action
+      },
+    },
+  )
+  return action
+}
+
+export type ActionHandler<
+  A = unknown,
+  E = any,
+  R = any,
+> = (bindings: any) => Effect.Effect<A, E, R>
+
+export namespace Action {
+  export interface Action<
+    out Handler extends ActionHandler = ActionHandler,
+    out Bindings extends Record<string, any> = Record<string, any>,
+  > extends ActionSet.ActionSet<[Action<Handler, Bindings>]> {
+    readonly handler: Handler
+  }
+
+  export type Default = Action<ActionHandler, Record<string, any>>
+
+  export type Tuple = readonly [Item, ...Item[]]
+
+  export type Array = ItemArray
+
+  export type Error<T> = T extends ActionSet.ActionSet<infer Items>
+    ? _ExtractError<Items>
+    : never
+
+  type _ExtractError<M extends ItemArray> = M extends readonly [
+    infer Head,
+    ...infer Tail extends ItemArray,
+  ] ? (
+      Head extends Action<infer H, any> ?
+          | (H extends ActionHandler<any, infer E, any> ? E : never)
+          | _ExtractError<Tail>
+        : Head extends ActionSet.ActionSet<infer Nested>
+          ? _ExtractError<Nested> | _ExtractError<Tail>
+        : _ExtractError<Tail>
+    )
+    : never
+
+  export type Requirements<T> = T extends ActionSet.ActionSet<infer Items>
+    ? _ExtractRequirements<Items>
+    : never
+
+  type _ExtractRequirements<M extends ItemArray> = M extends readonly [
+    infer Head,
+    ...infer Tail extends ItemArray,
+  ] ? (
+      Head extends Action<infer H, any> ?
+          | (H extends ActionHandler<any, any, infer R> ? R : never)
+          | _ExtractRequirements<Tail>
+        : Head extends ActionSet.ActionSet<infer Nested>
+          ? _ExtractRequirements<Nested> | _ExtractRequirements<Tail>
+        : _ExtractRequirements<Tail>
+    )
+    : never
+
+  export type Bindings<T> = T extends ActionSet.ActionSet<infer Items>
+    ? _ExtractBindings<Items>
+    : never
+
+  type _ExtractBindings<M extends ItemArray> = M extends readonly [
+    infer Head,
+    ...infer Tail extends ItemArray,
+  ] ? (
+      Head extends Action<any, infer B> ? B | _ExtractBindings<Tail>
+        : Head extends ActionSet.ActionSet<infer Nested>
+          ? _ExtractBindings<Nested> | _ExtractBindings<Tail>
+        : _ExtractBindings<Tail>
+    )
+    : never
+}
+
+export type Merge<
+  A extends ActionSet.Any,
+  B extends ActionSet.Any,
+> = ActionSet.ActionSet<readonly [A, B]>
+
+export const merge: {
+  <B extends ActionSet.Any>(
+    other: B,
+  ): <A extends ActionSet.Any>(
+    self: A,
+  ) => ActionSet.ActionSet<readonly [A, B]>
+
+  <A extends ActionSet.Any, B extends ActionSet.Any>(
+    self: A,
+    other: B,
+  ): ActionSet.ActionSet<readonly [A, B]>
+} = dual(2, <A extends ActionSet.Any, B extends ActionSet.Any>(
+  self: A,
+  other: B,
+): ActionSet.ActionSet<readonly [A, B]> => {
+  return make([self, other] as const)
+})
+
+export function mergeAll<
+  Sets extends readonly [ActionSet.Any, ...ActionSet.Any[]],
+>(
+  ...sets: Sets
+): ActionSet.ActionSet<Sets> {
+  return make(sets)
+}
+
+type Mutable<T> = { -readonly [K in keyof T]: T[K] } & {}
+
+export const schemaHeaders: {
+  <T extends Schema.Struct<any>>(
+    schema: T,
+  ): <M extends ItemArray>(
+    self: ActionSet.ActionSet<M>,
+  ) => ActionSet.ActionSet<
+    [
+      ...M,
+      Action.Action<
+        ActionHandler<void, never, never>,
+        { headers: Mutable<Schema.Schema.Type<T>> }
+      >,
+    ]
+  >
+
+  <M extends ItemArray, T extends Schema.Struct<any>>(
+    self: ActionSet.ActionSet<M>,
+    schema: T,
+  ): ActionSet.ActionSet<
+    [
+      ...M,
+      Action.Action<
+        ActionHandler<void, never, never>,
+        { headers: Mutable<Schema.Schema.Type<T>> }
+      >,
+    ]
+  >
+} = dual(
+  2,
+  <M extends ItemArray, T extends Schema.Struct<any>>(
+    self: ActionSet.ActionSet<M>,
+    _schema: T,
+  ): ActionSet.ActionSet<
+    [
+      ...M,
+      Action.Action<
+        ActionHandler<void, never, never>,
+        { headers: Mutable<Schema.Schema.Type<T>> }
+      >,
+    ]
+  > => {
+    const action = makeAction<
+      ActionHandler<void, never, never>,
+      { headers: Mutable<Schema.Schema.Type<T>> }
+    >(() => Effect.void)
+
+    return make([...items(self), action] as const)
+  },
+)
+
+type ExtractBindings<M extends ItemArray> = M extends readonly [
+  infer Head,
+  ...infer Tail extends ItemArray,
+] ? (
+    Head extends Action.Action<any, infer B> ? B & ExtractBindings<Tail>
+      : Head extends ActionSet.ActionSet<infer Nested>
+        ? ExtractBindings<Nested> & ExtractBindings<Tail>
+      : ExtractBindings<Tail>
+  )
+  : {}
+
+function makeMethodAction<M extends ActionPattern["method"]>(_method: M) {
+  return <R extends ActionSet.Any>(
+    f: (
+      self: ActionSet.ActionSet<
+        readonly [
+          Action.Action<ActionHandler<void, never, never>, { method: M }>,
+        ]
+      >,
+    ) => R,
+  ): R => {
+    const methodAction = makeAction<
+      ActionHandler<void, never, never>,
+      { method: M }
+    >(() => Effect.void)
+    const set = make([methodAction] as const)
+    return f(set)
+  }
+}
+
+export const get = makeMethodAction("GET")
+export const post = makeMethodAction("POST")
+
+export const text: {
+  <M extends ItemArray, A, E, R>(
+    handler: (bindings: ExtractBindings<M>) => Effect.Effect<A, E, R>,
+  ): (
+    self: ActionSet.ActionSet<M>,
+  ) => ActionSet.ActionSet<
+    [...M, Action.Action<ActionHandler<A, E, R>, ExtractBindings<M>>]
+  >
+
+  <M extends ItemArray, A, E, R>(
+    self: ActionSet.ActionSet<M>,
+    handler: (bindings: ExtractBindings<M>) => Effect.Effect<A, E, R>,
+  ): ActionSet.ActionSet<
+    [...M, Action.Action<ActionHandler<A, E, R>, ExtractBindings<M>>]
+  >
+} = dual(2, <M extends ItemArray, A, E, R>(
+  self: ActionSet.ActionSet<M>,
+  handler: (bindings: ExtractBindings<M>) => Effect.Effect<A, E, R>,
+): ActionSet.ActionSet<
+  [...M, Action.Action<ActionHandler<A, E, R>, ExtractBindings<M>>]
+> => {
+  const action = makeAction<ActionHandler<A, E, R>, ExtractBindings<M>>(
+    handler as ActionHandler<A, E, R>,
+  )
+
+  return make([...items(self), action] as const)
+})
