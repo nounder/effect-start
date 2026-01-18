02e299d213bc768f36541a6afd4bd7168803ac58
Rafael Gutkowski <nounderline@libred.org>
2026-01-18 20:54:05 +0100

    Values.Json: allow undefined values

--- src/Values.ts
+++ src/Values.ts
@@ -8,5 +8,9 @@ export type Json =
   | JsonPrimitives
   | Json[]
   | {
-    [key: string]: Json
+    [key: string]:
+      | Json
+      // undefined won't be included in JSON objects but this will allow
+      // to use Json type in functions that return object of multiple shapes
+      | undefined
   }
