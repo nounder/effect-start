a9248dc5814baa56dd19b3f0880659be48bc0f84
Rafael Gutkowski <nounderline@libred.org>
2026-02-05 00:08:04 +0100

    BunRuntime: clean up on hot-reload

--- src/bun/BunRuntime.ts
+++ src/bun/BunRuntime.ts
@@ -1,9 +1,24 @@
+import type * as Fiber from "effect/Fiber"
+import * as GlobalValue from "effect/GlobalValue"
+import * as MutableRef from "effect/MutableRef"
 import * as PlatformRuntime from "../PlatformRuntime.ts"
+
+const mainFiber = GlobalValue.globalValue(
+  Symbol.for("effect-start/BunRuntime/existingFiber"),
+  () =>
+    MutableRef.make<Fiber.RuntimeFiber<unknown, unknown> | undefined>(
+      undefined,
+    ),
+)
 
 export const runMain = PlatformRuntime.makeRunMain(({
   fiber,
   teardown,
 }) => {
+  const prevFiber = MutableRef.get(mainFiber)
+
+  MutableRef.set(mainFiber, fiber)
+
   let receivedSignal = false
 
   fiber.addObserver((exit) => {
@@ -27,4 +42,8 @@ export const runMain = PlatformRuntime.makeRunMain(({
 
   process.on("SIGINT", onSigint)
   process.on("SIGTERM", onSigint)
+
+  if (prevFiber) {
+    prevFiber.unsafeInterruptAsFork(prevFiber.id())
+  }
 })
