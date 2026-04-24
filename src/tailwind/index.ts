9884a67a4593428be8fb2a444f94d119e64dd6e9
Rafael Gutkowski <nounderline@libred.org>
2026-04-24 15:02:49 +0200

    tailwind: honor tailwind query param config

--- src/tailwind/index.ts
+++ src/tailwind/index.ts
@@ -1,2 +1,10 @@
-export { default } from "./plugin.ts"
+import * as TailwindPlugin from "./TailwindPlugin.ts"
+import { scanPathFromUrl } from "./plugin.ts"
+
 export * as TailwindPlugin from "./TailwindPlugin.ts"
+
+const scanPath = await scanPathFromUrl(import.meta.url)
+
+export default TailwindPlugin.make({
+  scanPath,
+})
--- src/tailwind/plugin.ts
+++ src/tailwind/plugin.ts
@@ -2,16 +2,18 @@ import * as NPath from "node:path"
 import * as NodeUtils from "../node/NodeUtils.ts"
 import * as TailwindPlugin from "./TailwindPlugin.ts"
 
-// Append `?dir=` to module identifier to pass custom directory to scan
-const dirParam = URL.parse(import.meta.url)?.searchParams.get("dir")
-const packageJson = await NodeUtils.findClosestPackageJson(process.cwd())
-const scanPath = dirParam
-  ? NPath.resolve(process.cwd(), dirParam)
-  : packageJson
-    ? NPath.dirname(packageJson)
-    : process.cwd()
+export const scanPathFromUrl = async (url: string) => {
+  const dirParam = URL.parse(url)?.searchParams.get("dir")
+  const packageJson = await NodeUtils.findClosestPackageJson(process.cwd())
+
+  return dirParam
+    ? NPath.resolve(process.cwd(), dirParam)
+    : packageJson
+      ? NPath.dirname(packageJson)
+      : process.cwd()
+}
 
 // Export as default to be used in bunfig.toml
 export default TailwindPlugin.make({
-  scanPath,
+  scanPath: await scanPathFromUrl(import.meta.url),
 })
